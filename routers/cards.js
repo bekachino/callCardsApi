aed4708f7c0b43208c2fcaffaa1e8298b4f453f8
Bektur Razzakov <brzzkv@gmail.com>
2025-03-16 23:38:00 +0600

    get cards with reason and solution titles

--- routers/cards.js
+++ routers/cards.js
@@ -6,14 +6,32 @@ const cardsRouter = express();
 
 cardsRouter.get('/', (req, res) => {
   try {
-    const { start_date, end_date } = req.query;
+    const {
+      start_date,
+      end_date
+    } = req.query;
     let sql;
     let sqlParams = [];
     
     if (!start_date && !end_date) {
       sql = 'SELECT * FROM cards';
     } else {
-      sql = `SELECT * FROM cards WHERE ${!!start_date ? 'created_at >= ?' : ''} ${!!start_date && !!end_date ? 'AND' : ''} ${!!end_date ? 'created_at <= ?' : ''}`;
+      sql = `
+        select
+         C.id,
+         C.ls_abon,
+         C.created_at,
+         C.spec_full_name,
+         C.sip,
+         C.full_name,
+         C.phone_number,
+         C.address,
+         C.comment,
+         R.title as reason, S.title as solution from cards as C
+        left join reasons as R on R.id = C.reason_id
+        left join solutions as S on S.id = C.solution_id
+        WHERE ${!!start_date ? 'C.created_at >= ?' : ''} ${!!start_date && !!end_date ? 'AND' : ''} ${!!end_date ? 'C.created_at <= ?' : ''}
+      `;
       if (!!start_date) sqlParams.push(start_date);
       if (!!end_date) sqlParams.push(end_date);
     }
