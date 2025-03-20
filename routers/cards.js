f4a40afad44bc94b318e500e4a1cf225667d35cf
Bektur Razzakov <brzzkv@gmail.com>
2025-03-20 16:27:30 +0600

    route for repeated calls

--- routers/cards.js
+++ routers/cards.js
@@ -4,6 +4,29 @@ import { ERROR_MESSAGES, initialGetCardsSql } from "../constants.js";
 
 const cardsRouter = express();
 
+const formatRepeatedCalls = rawData => {
+  const grouped = {};
+  
+  rawData.forEach(item => {
+    const key = `${item.ls_abon}|${item.reason}|${item.solution || 'null'}`;
+    
+    if (!grouped[key]) {
+      grouped[key] = {
+        count: 0,
+        ls_abon: item.ls_abon,
+        address: item.address,
+        phone_number: item.phone_number,
+        reason: item.reason,
+        solution: item.solution || null,
+      };
+    }
+    
+    grouped[key].count += 1;
+  });
+  
+  return Object.values(grouped);
+};
+
 cardsRouter.get('/', (req, res) => {
   try {
     const {
@@ -39,7 +62,12 @@ cardsRouter.get('/', (req, res) => {
     
     db.all(sql, sqlParams, (err, rows) => {
       if (err) return res.status(500).json({ error: err.message });
-      res.json(rows.map(row => ({...row, phone_number: JSON.parse(row.phone_number) })));
+      res.json(rows.map(row => (
+        {
+          ...row,
+          phone_number: JSON.parse(row.phone_number)
+        }
+      )));
     });
   } catch (e) {
     res.status(500).send(e.message);
@@ -197,6 +225,56 @@ cardsRouter.get('/report', (req, res) => {
         }
       });
       res.json(Object.keys(result).map(sipKey => result[sipKey]));
+    });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+cardsRouter.get('/repeated_calls', (req, res) => {
+  try {
+    const {
+      start_date,
+      end_date
+    } = req.query;
+    
+    let sql;
+    let sqlParams = [];
+    
+    if (!start_date && !end_date) {
+      sql = initialGetCardsSql;
+    } else {
+      sql = `
+        select
+         C.id,
+         C.ls_abon,
+         C.created_at,
+         C.spec_full_name,
+         C.sip,
+         C.full_name,
+         C.phone_number,
+         C.address,
+         C.comment,
+         R.title as reason, S.title as solution from cards as C
+        left join reasons as R on R.id = C.reason_id
+        left join solutions as S on S.id = C.solution_id
+        WHERE ${!!start_date ? 'C.created_at >= ?' : ''} ${!!start_date && !!end_date ? 'AND' : ''} ${!!end_date ? 'C.created_at <= ?' : ''}
+      `;
+      if (!!start_date) sqlParams.push(start_date);
+      if (!!end_date) sqlParams.push(end_date);
+    }
+    
+    db.all(sql, sqlParams, (err, rows) => {
+      if (err) return res.status(500).json({ error: err.message });
+      
+      const rawData = rows.map(row => (
+        {
+          ...row,
+          phone_number: JSON.parse(row.phone_number)
+        }
+      ));
+      const groupedData = formatRepeatedCalls(rawData);
+      res.send(groupedData);
     });
   } catch (e) {
     res.status(500).send(e.message);
